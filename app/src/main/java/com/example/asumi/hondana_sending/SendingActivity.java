4ec6aeb5620a2006d854c1bd5ad5fea5d364abe0
門脇明日美 <asumi.kadowaki1018@gmail.com>
2016-08-09 13:58:46 +0900

    Initial commit

---
+++ app/src/main/java/com/example/asumi/hondana_sending/SendingActivity.java
@@ -0,0 +1,13 @@
+package com.example.asumi.hondana_sending;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SendingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sending);
+    }
+}
